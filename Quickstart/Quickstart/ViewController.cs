9298bebfbc160d575ce8b27912d8e29c096dbe84
luckgagan <luck.gagan@gmail.com>
2020-03-08 21:37:55 -0400

    Puhsing first change set.

---
+++ Quickstart/Quickstart/ViewController.cs
@@ -0,0 +1,57 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace WordWorld
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+            // Perform any additional setup after loading the view, typically from a nib.
+            string translatedNumber = string.Empty;
+
+            btnTranslate.TouchUpInside += (object sender, EventArgs e) => 
+            {
+                translatedNumber = PhoneTranslator.ToNumber(PhoneNumberText.Text);
+                PhoneNumberText.ResignFirstResponder();
+
+                if (string.IsNullOrEmpty(translatedNumber))
+                {
+                    btnCall.SetTitle("Call", UIControlState.Normal);
+                    btnCall.Enabled = false;
+                }
+                else
+                {
+                    btnCall.SetTitle("Call " + translatedNumber, UIControlState.Normal);
+                    btnCall.Enabled = true;
+                }
+            };
+
+
+            btnCall.TouchUpInside += (object sender, EventArgs e) => 
+            {
+                var url = new NSUrl("tel:" + translatedNumber);
+
+                if(!UIApplication.SharedApplication.OpenUrl(url))
+                {
+                    var alert = UIAlertController.Create("Not supported", "Scheme 'tel:' is not supported on this device", UIAlertControllerStyle.Alert);
+                    alert.AddAction(UIAlertAction.Create("Ok", UIAlertActionStyle.Default, null));
+                    PresentViewController(alert, true, null);
+                }
+            };
+
+        }
+
+        public override void DidReceiveMemoryWarning ()
+        {
+            base.DidReceiveMemoryWarning ();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ Quickstart/Quickstart/ViewController.designer.cs
@@ -0,0 +1,47 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace WordWorld
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnCall { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnTranslate { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField PhoneNumberText { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (btnCall != null) {
+                btnCall.Dispose ();
+                btnCall = null;
+            }
+
+            if (btnTranslate != null) {
+                btnTranslate.Dispose ();
+                btnTranslate = null;
+            }
+
+            if (PhoneNumberText != null) {
+                PhoneNumberText.Dispose ();
+                PhoneNumberText = null;
+            }
+        }
+    }
+}
