885c70f15c0a034b0ae1fe14c82fe13fcc05dd38
luckgagan <luck.gagan@gmail.com>
2020-07-12 22:01:08 -0400

    test commit.

--- ShabdkoshApp/ShabdkoshApp/Views/WordsCollection.xaml.cs
+++ ShabdkoshApp/ShabdkoshApp/Views/WordsCollection.xaml.cs
@@ -1,11 +1,5 @@
 ﻿using ShabdkoshApp.Models;
 using ShabdkoshApp.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
