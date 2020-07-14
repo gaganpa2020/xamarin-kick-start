bce16a453468cf6a4b18fe1b5e39eca8f0e865b8
luckgagan <luck.gagan@gmail.com>
2020-07-13 21:37:19 -0400

    Changing icon.

--- ShabdkoshApp/ShabdkoshApp.Android/Resources/Resource.designer.cs
+++ ShabdkoshApp/ShabdkoshApp.Android/Resources/Resource.designer.cs
@@ -5788,13 +5788,13 @@ namespace ShabdkoshApp.Droid
 			public const int notify_panel_notification_icon_bg = 2131165299;
 			
 			// aapt resource value: 0x7F070074
-			public const int tooltip_frame_dark = 2131165300;
+			public const int shabdkosh_logo = 2131165300;
 			
 			// aapt resource value: 0x7F070075
-			public const int tooltip_frame_light = 2131165301;
+			public const int tooltip_frame_dark = 2131165301;
 			
 			// aapt resource value: 0x7F070076
-			public const int xamarin_logo = 2131165302;
+			public const int tooltip_frame_light = 2131165302;
 			
 			static Drawable()
 			{
